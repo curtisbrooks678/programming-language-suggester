daeb6a99e8d81a1c31eac60e08d5338725eb5779
Curtis Brooks <curtisbrooks678@gmail.com>
2021-10-22 10:04:26 -0700

    add if logic to add to quiz counters.

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,73 @@ $(document).ready(function() {
     const ans4 = $("input:radio[name=dessert]:checked").val();
     const ans5 = $("input:radio[name=vegetable]:checked").val();
 
+    let ruby = 0
+    let csharp = 0;
+    let javascript = 0;
 
-    let ruby;
-    let csharp;
-    let javascript;
+    if (ans1 === "red") {
+      ruby += 1;
+    } else if (ans1 === "yellow") {
+      csharp += 1;
+    } else if (ans1 === "blue") {
+      javascript += 1;
+    } else {
+      alert("Error: Please select an option for the first question.");
+    }
 
+    if (ans2 === "sunny") {
+      ruby += 1;
+    } else if (ans2 === "rainy") {
+      csharp += 1;
+    } else if (ans2 === "snowy") {
+      javascript += 1;
+    } else {
+      alert("Error: Please select an option for the second question.");
+    }
+
+    if (ans3 === "classical") {
+      ruby += 1;
+    } else if (ans3 === "rock") {
+      csharp += 1;
+    } else if (ans3 === "bossa-nova") {
+      javascript += 1;
+    } else {
+      alert("Error: Please select an option for the third question.");
+    }
+
+    if (ans3 === "classical") {
+      ruby += 1;
+    } else if (ans3 === "rock") {
+      csharp += 1;
+    } else if (ans3 === "bossa-nova") {
+      javascript += 1;
+    } else {
+      alert("Error: Please select an option for the third question.");
+    }
+
+    if (ans4 === "ice-cream") {
+      ruby += 1;
+    } else if (ans4 === "cake") {
+      csharp += 1;
+    } else if (ans4 === "cookies") {
+      javascript += 1;
+    } else {
+      alert("Error: Please select an option for the fourth question.");
+    }
+
+    if (ans5 === "broccoli") {
+      ruby += 1;
+    } else if (ans5 === "kale") {
+      csharp += 1;
+    } else if (ans5 === "carrots") {
+      javascript += 1;
+    } else {
+      alert("Error: Please select an option for the fifth question.");
+    }
+
+
+
+    
 
 
     $("#output").text(result);
